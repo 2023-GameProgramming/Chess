66b830c43a455349a6e1ec1ea662b62629e7cc39
tjdvlf2880 <zx2880@naver.com>
2023-05-22 21:30:51 +0900

    사운드 및 ui 추가

--- Assets/Scripts/HatUI.cs
+++ Assets/Scripts/HatUI.cs
@@ -5,9 +5,12 @@ using UnityEngine.UI;
 
 public class HatUI : MonoBehaviour
 {
+    float lerp;
+    int dir = 1;
     // Start is called before the first frame update
     void Start()
     {
+        lerp = 0;
         transform.Find("Front").GetComponent<Image>().sprite= ResourceManager.Instance.ImageList["Front.PNG"];
         transform.Find("Back").GetComponent<Image>().sprite = ResourceManager.Instance.ImageList["Back.PNG"];
         transform.Find("queenSlot").GetComponent<Image>().sprite = ResourceManager.Instance.ImageList["queen.PNG"];
@@ -16,13 +19,22 @@ public class HatUI : MonoBehaviour
         transform.Find("rookSlot").GetComponent<Image>().sprite = ResourceManager.Instance.ImageList["rook.PNG"];
         transform.Find("knightSlot").GetComponent<Image>().sprite = ResourceManager.Instance.ImageList["knight.PNG"];
         transform.Find("kingSlot").GetComponent<Image>().sprite = ResourceManager.Instance.ImageList["king.PNG"];
-        transform.Find("kingSlot").GetComponent<Image>().color = Color.white;
+        transform.Find("kingSlot").GetComponent<Image>().color = Color.magenta;
         transform.Find("CurPieceSlot").GetComponent<Image>().sprite = ResourceManager.Instance.ImageList["king.PNG"]; ;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        lerp += dir*Time.deltaTime;
+        if(lerp>1)
+        {
+            dir = -1;
+        }
+        else if(lerp<0)
+        {
+            dir = 1;
+        }
+        transform.Find("CurPieceSlot").GetComponent<Image>().color = Color.Lerp(Color.white, Color.magenta, lerp);
     }
 }
